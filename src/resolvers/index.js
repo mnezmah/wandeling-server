e4249091a9384bcbbcb1100b3e2f02ec0d894676
mario <marionezmah@kateinnovations.com>
2021-05-24 13:22:10 +0200

    clean start

--- src/resolvers/index.js
+++
@@ -1,9 +0,0 @@
-import { GraphQLDateTime } from "graphql-iso date";
-const customResolver = {
-    Date: GraphQLDateTime,
-};
-import { GraphQLDateTime } from "graphql-iso-date";
-import userResolver from "./user";
-import mkarkerResolver from "./user";
-
-export default [customResolver, userResolver, mkarkerResolver];
